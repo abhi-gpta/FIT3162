2feaf9f548d104b3b14884392493c440153162ff
lordcelp <lordcelpz@gmail.com>
2023-10-15 16:39:28 +1100

    restructure

---
+++ Database Reading/cpi_reader.py
@@ -0,0 +1,157 @@
+def main(file_path):
+   
+    categories = ['Lamb and goat',
+                'Wine',
+                'Food additives and condiments',
+                'Other meats',
+                'Garments for men',
+                'Cheese',
+                'Garments',
+                'Bread',
+                'Fruit',
+                'Footwear for men',
+                'Ice cream and other dairy products',
+                'Meals out and take away foods',
+                'Poultry',
+                'Restaurant meals',
+                'Glassware, tableware and household utensils',
+                'Garments for women',
+                'Other non-durable household products',
+                'Beer',
+                'Eggs',
+                'Oils and fats',
+                'Waters, soft drinks and juices',
+                'Other food products n.e.c.',
+                'Food products n.e.c.',
+                'Footwear for women',
+                'Meat and seafoods',
+                'Snacks and confectionery',
+                'Beef and veal',
+                'Take away and fast foods',
+                'Other cereal products',
+                'Cakes and biscuits',
+                'Coffee, tea and cocoa',
+                'Jams, honey and spreads',
+                'Books',
+                'Dairy and related products',
+                'Tobacco',
+                'Alcohol and tobacco',
+                'Pork',
+                'Garments for infants and children',
+                'Clothing and footwear',
+                'Bread and cereal products',
+                'Personal care products',
+                'Breakfast cereals',
+                'Fish and other seafood',
+                'Non-alcoholic beverages',
+                'Spirits',
+                'Non-durable household products',
+                'Footwear for infants and children',
+                'Cleaning and maintenance products',
+                'Newspapers, books and stationery',
+                'Milk',
+                'Household appliances, utensils and tools',
+                'Newspapers, magazines and stationery',
+                'Vegetables',
+                'Footwear',
+                'Alcoholic beverages',
+                'Fruit and vegetables',
+                'Furniture']
+
+    xls = pd.ExcelFile(file_path)
+    sheet_names = xls.sheet_names[2:-1]  # skip the first 2 sheets and the last one
+
+    # Add first sheet manually
+    combined_df = pd.read_excel(xls, xls.sheet_names[1])
+    # Read each sheet and concatenate it horizontally to the combined DataFrame
+    for sheet_name in sheet_names:
+        sheet_df = pd.read_excel(xls, sheet_name)
+        sheet_df = sheet_df.drop(sheet_df.columns[0], axis=1)
+        combined_df = pd.concat([combined_df, sheet_df], axis=1)
+
+    combined_df.iloc[0] = combined_df.columns
+    combined_df = combined_df.drop([2,3,4,5,6,7,8], axis=0)
+
+    header_row = combined_df.iloc[0]
+
+    # Split the first row into three separate values using ';'
+    new_header = header_row.str.split(';').tolist()
+    new_header[0] = ['Date', np.NaN, np.NaN]
+    for i in range(1,len(new_header)):
+        new_header[i] = new_header[i][1:3]
+        for j in range(2):
+            new_header[i][j] = new_header[i][j].strip()
+    new_header = list(map(list, zip(*new_header)))
+    new_header = pd.DataFrame(new_header)
+    new_header = new_header.reset_index(drop=True)
+
+    # Create a new DataFrame with the split values as the first row
+    combined_df = combined_df.drop([0,1],axis=0)
+    combined_df = combined_df.reset_index(drop=True)
+    combined_df.columns = new_header.columns
+    combined_df = pd.concat([new_header, combined_df], axis=0, ignore_index=True)
+    combined_df.columns = combined_df.iloc[0]
+    combined_df=combined_df.drop([0], axis = 0)
+
+    filtered_df = combined_df[categories + ["Date"]]
+    filtered_df['Date'] = pd.to_datetime(filtered_df['Date'])
+    cols = ['Date'] + [col for col in filtered_df if col != 'Date']
+    filtered_df = filtered_df[cols]
+
+    transposed_df = filtered_df.T
+    transposed_df = transposed_df.reset_index()
+    dates = list(transposed_df.iloc[0][2:])
+    transposed_df.columns = ['Product','City'] + dates
+    transposed_df = transposed_df.drop([0])
+
+    df_melted = transposed_df.melt(id_vars=['Product', 'City'], 
+                        value_vars=dates,
+                        var_name='Date', value_name='Index Value')
+
+   # df_melted["% Change"] = df_melted['Index Value'].pct_change() * 100
+    df_melted.to_csv('output.csv')
+    upload_to_database(df_melted)
+    
+
+def upload_to_database(df):
+    db_config = {
+        "host": "fit3162.mysql.database.azure.com",
+        "user": "fit3162",
+        "password": "Consumercare123!",
+        "database": "fit3162",
+        "port": 3306
+    }
+
+    # Create a SQLAlchemy engine
+    engine = create_engine(f"mysql+pymysql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}")
+
+    # Get the table name
+    table_name = 'cpi_data'
+
+    # Create the table based on the dataframe structure
+    df.head(0).to_sql(name=table_name, con=engine, if_exists='replace', index=False)
+
+    # Insert the dataframe records into the table (replace if it exists)
+    df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
+
+
+if __name__ == "__main__":
+    import pandas as pd
+    import numpy as np
+    from sqlalchemy import create_engine
+    import sys
+    import os
+    arguments = sys.argv
+    arg1 = arguments[1]
+    file_path = arg1
+    
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"The file or directory '{file_path}' does not exist.")
+    
+    file_extension = os.path.splitext(file_path)[1].lower()
+    excel_extensions = ['.xls', '.xlsx', '.xlsm']
+    if file_extension not in excel_extensions:
+        raise ValueError(f"The file '{file_path}' is not an Excel file (supported extensions: {', '.join(excel_extensions)}).")
+    
+    print(f"File {file_path} was found")
+    main(file_path)
---
+++ Website/regression.py
@@ -0,0 +1,95 @@
+def check_data(file):
+    import pandas as pd
+    import os.path
+    load = False
+    if os.path.isfile(file): 
+        with open(file, 'r') as csvfile:
+            first_line = csvfile.readline().strip()
+            # Check if the first line matches the expected header
+            if first_line != "Product,City,Date,Index Value":
+                load = True
+                print('Invalid local file')
+    
+    if os.path.isfile(file) and not load:
+        df = pd.read_csv(file)
+    else:
+        from load_data import loaddata
+        print("Loading from database")
+        loaddata()
+        df = pd.read_csv(file)
+    return df
+
+def regress(product):
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import sklearn
+    import xgboost as xgb
+    from sklearn.metrics import mean_squared_error
+    from sklearn.linear_model import LinearRegression
+    color_pal = sns.color_palette()
+    import os.path
+
+    plt.style.use('bmh')
+    
+    df = check_data('output.csv')
+
+    
+    # Check if the product exists in the DataFrame
+    if product not in df['Product'].values:
+        raise ValueError(f"The product '{product}' does not exist in the database.")
+
+    
+    df = df[df['Product'] == product]
+    
+    df = df.groupby(['Date'])['Index Value'].mean().reset_index()
+    df = df.sort_values(by='Date')
+    df = df.set_index('Date')
+    df.index = pd.to_datetime(df.index)
+    df = df[df['Index Value'].notna()]
+    
+    train = df.loc[df.index < '01-01-2016']
+    train = df.loc[df.index > '01-01-2000']
+    test = df.loc[df.index >= '01-01-2016']
+    
+    last_date = test.index[-1]
+    for i in range(10):
+        for j in range(4):
+            last_date += pd.DateOffset(months=3)
+            test.loc[last_date] = np.NaN
+    
+    def create_features(df):
+        df = df.copy()
+        df['quarter'] = df.index.quarter
+        df['year'] = df.index.year
+        df1 = pd.get_dummies(df, columns=['quarter'], prefix=['quarter'])
+        df1['quarter'] = df.index.quarter
+        return df1
+    df = create_features(df)
+
+    train = create_features(train)
+    test = create_features(test)
+
+    FEATURES = ['year', 'quarter_1','quarter_2','quarter_3','quarter_4']	
+    TARGET = 'Index Value'
+
+    X_train = train[FEATURES]
+    y_train = train[TARGET]
+
+    X_test = test[FEATURES]
+    y_test = test[TARGET]
+    
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    test['prediction'] = model.predict(X_test)
+    df = df.merge(test[['prediction']], how='outer', left_index=True, right_index=True)
+    plt.tight_layout()
+
+    ax = df[['Index Value']].plot(figsize=(15, 5))
+    ax.set_ylabel('Index Points')
+    df['prediction'].plot(ax=ax, style='-')
+    plt.legend(['Real Data', 'Predictions'])
+    ax.set_title('Forecasted Relative Price for: ' + product)
+    plt.savefig('static/plot.png', bbox_inches='tight', format='png')
+    plt.close()
